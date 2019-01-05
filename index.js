897ab59a29335af244ac3c2e641a8c6be1f311d3
wibastidas <wibastidas77@gmail.com>
2019-01-05 18:23:59 -0300

    add transition link inter

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+$(function(){
+  $('a[href*=\\#]').click(function() {
+
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      && location.hostname == this.hostname) {
+
+          var $target = $(this.hash);
+
+          $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+          if ($target.length) {
+
+              var targetOffset = $target.offset().top;
+
+              $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+              return false;
+
+         }
+
+    }
+
+});
+
+});
+
+
